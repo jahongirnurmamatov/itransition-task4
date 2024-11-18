f23b8547ca94ff47c3279105038a030d13b2d475
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-18 21:39:22 +0500

    deployment modifications

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,14 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from 'path';
+
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const  __dirname = path.resolve();
+
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
@@ -17,9 +21,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT = ${PORT}`);
